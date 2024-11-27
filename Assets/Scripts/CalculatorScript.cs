f2fdfe38fffe2704159d604a18c70b25f62c2d39
Misha_Unity585 <miha150908@gmail.com>
2024-11-27 18:05:08 +0300

    changed the code so that it can divide by zero

--- Assets/Scripts/CalculatorScript.cs
+++ Assets/Scripts/CalculatorScript.cs
@@ -3,41 +3,44 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class CalculatorScript : MonoBehaviour 
-{
+public class CalculatorScript : MonoBehaviour  {
     [SerializeField] private TMP_InputField inputField1;
     [SerializeField] private TMP_InputField inputField2;
     [SerializeField] private TMP_Text outputText;
- 
+
     public void OnButtonMinus() {
-        int value1 = ReadIntFromInputField(inputField1);
-        int value2 = ReadIntFromInputField(inputField2);
+        float value1 = ReadFloatFromInputField(inputField1);
+        float value2 = ReadFloatFromInputField(inputField2);
         outputText.text = (value1 - value2).ToString();
     }
 
-    public void OnButtonPlus(){
-        int value1 = ReadIntFromInputField(inputField1);
-        int value2 = ReadIntFromInputField(inputField2);
+    public void OnButtonPlus() {
+        float value1 = ReadFloatFromInputField(inputField1);
+        float value2 = ReadFloatFromInputField(inputField2);
         outputText.text = (value1 + value2).ToString();
     }
 
-    public void OnButtonDiv(){
-        int value1 = ReadIntFromInputField(inputField1);
-        int value2 = ReadIntFromInputField(inputField2);
-        outputText.text = (value1 / value2).ToString();
+    public void OnButtonDiv() {
+        float value1 = ReadFloatFromInputField(inputField1);
+        float value2 = ReadFloatFromInputField(inputField2);
+        if (value2 == 0) {
+            outputText.text = "Не делится";
+        } else {
+            outputText.text = (value1 / value2).ToString();
+        }
     }
 
-    public void OnButtonMulti(){
-        int value1 = ReadIntFromInputField(inputField1);
-        int value2 = ReadIntFromInputField(inputField2);
+    public void OnButtonMulti() {
+        float value1 = ReadFloatFromInputField(inputField1);
+        float value2 = ReadFloatFromInputField(inputField2);
         outputText.text = (value1 * value2).ToString();
     }
 
-    private int ReadIntFromInputField(TMP_InputField inputField) {
+    private float ReadFloatFromInputField(TMP_InputField inputField) {
         string str = inputField.text;
         if (str != null && str.Length > 0) {
-            if (int.TryParse(str, out int intValue)) {
-                return intValue;
+            if (float.TryParse(str, out float floatValue)) {
+                return floatValue;
             }
         }
         return 0;
